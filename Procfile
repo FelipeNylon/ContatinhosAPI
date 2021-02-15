c86aa878d79f4b7107013c91daa6a3f998b179b5
Felipe Marlon <mr.sheldonvs@gmail.com>
2021-02-15 16:43:06 -0300

    ch(deploy) Setup deploy to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn contatinhosapi.wsgi
--- contatinhosapi/settings.py
+++ contatinhosapi/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from var import *
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -147,3 +149,4 @@ JWT_SECRET_KEY= os.environ["JWT_SECRET_KEY"]
 STATIC_URL = '/static/'
 EDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+django_heroku.settings(locals())
